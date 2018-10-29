8dd1405bf534830f77b0ab382cf0bac3707dbf15
KolbeinnTDK <kolliingi@gmail.com>
2018-10-29 11:46:18 +0000

    P1

---
+++ P1/sketch.js
@@ -0,0 +1,12 @@
+// Hér kemur kóðinn þinn:
+function setup(){
+    createCanvas(400,300);
+    background(255,0,0);
+    fill(150);
+    ellipse(100,100,50,50);
+    fill(100,20,0);
+    textSize(20);
+    text("Halló heimur",200,40);
+    rect(95,160,10,20)
+    ellipse(100,200,50,50);
+}
